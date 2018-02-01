6e89197a388571a88e72d9ae873e0fc0521a9fa0
harryschmach <harrisons@sandiego>
2018-02-01 15:10:55 -0800

    I did it all. I think... - This is bigger

--- app/src/main/java/com/example/harryschmach/myowncard/MainActivity.kt
+++ app/src/main/java/com/example/harryschmach/myowncard/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.harryschmach.myowncard
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View.GONE
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,21 +11,26 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_bday -> {
-                greeting.setText(R.string.title_bday)
+                greeting.setText(R.string.greeting_bday)
                 card_image.setImageResource(R.drawable.birthdays)
                 bottom_text.setText(R.string.bottom_bday)
+                greeting.visibility  = android.view.View.VISIBLE
+                bottom_text.visibility = android.view.View.VISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_congrats -> {
-                greeting.setText(R.string.title_congrats)
-                card_image.setImageResource(R.drawable.confetti_girl)
+                greeting.setText(R.string.greeting_congrats)
+                card_image.setImageResource(R.drawable.confettigirl)
                 bottom_text.setText(R.string.bottom_congrats)
+                greeting.visibility  = android.view.View.VISIBLE
+                bottom_text.visibility = android.view.View.VISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_GwG2018 -> {
                 greeting.setText(R.string.title_just_cuz)
-                card_image.setImageResource(R.drawable.GrowWithGoogleDeveloperChallengeScholarship)
-                bottom_text.setText(R.string.bottom_just_cuz)
+                greeting.visibility = GONE
+                card_image.setImageResource(R.drawable.growwithgoogledeveloperchallengescholarship)
+                bottom_text.visibility = GONE
                 return@OnNavigationItemSelectedListener true
             }
         }
